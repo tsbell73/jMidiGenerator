effb52f5e7ab7a1ee207ab4bf4be9c4593047e17
Travis <Travis@Travis-PC>
2015-11-30 16:18:51 -0500

    Main class edited to transpose octaves chromatically to avoid crossing clefs.

--- src/jMidi.java
+++ src/jMidi.java
@@ -11,17 +11,23 @@ public class jMidi {
         for(int i=0; i < pitches.length; i++){
             // EBL1 = 40 CTL2 = 84 1==1/12 Chromatic Key
             pitches[i] = (50+((i+1)^i)^(i/2));
-            if(pitches[i] > 90){
-            pitches[i] = pitches[i] /(3);
+            if(pitches[i] > 82){
+          //  pitches[i] = pitches[i] /(3);
+                while(pitches[i] > 82){
+                pitches[i] = pitches[i] - 12;
+                }
             }
-            else if(pitches[i] < 40) {
-                pitches[i] = (pitches[i]+12)*2;
+            else if(pitches[i] < 60) {
+           //     pitches[i] = (pitches[i]+12)*2;           
+                while(pitches[i] < 60){
+                pitches[i] = pitches[i] + 12;
+                }
             }
-            Note n = new Note(pitches[i], .125);
+            Note n = new Note(pitches[i], .25);
             phrase.addNote(n);
         }
         
-        Play.midi(phrase);
+       // Play.midi(phrase);
         Write.midi(phrase, "midifile.mid");   
 
         }   
